35d6a16467599da77357fd3db2d757d6bd664290
Sarfaraj014 <sarfarajhussain692@gmail.com>
2025-12-30 22:54:19 +0530

    doing major updates

--- dice_code.py
+++ dice_code.py
@@ -1,3 +1,10 @@
+# Game Description
+# I have developed a simple game that allows users to play Ludo without a physical dice by using 
+# a computer-generated dice system.
+
+# The player only needs to enter a dice number, and the computer will automatically generate 
+# random dice points.
+# To exit the game, type 0 and press Enter.
 import random
 import os
 dicelist= ('''
@@ -38,7 +45,7 @@ dicelist= ('''
          └─────────┘ 
 ''')
 while True:
-    userinput=input("how many dice do you want")
+    userinput=input("How many dice do you want : ")
     os.system("clear")
     if int(userinput) == 0:
         break
